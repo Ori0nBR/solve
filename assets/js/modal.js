8bcf47eaf369200fc40340e709fde189d78ed2a1
victordeo <victorde.oliveira@icloud.com>
2025-02-09 17:33:53 -0300

    Modal

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const photos = document.querySelectorAll('.section-projects--photo');
     const modals = document.querySelectorAll('.g-m-1, .g-m-2, .g-m-3, .g-m-4, .g-m-5, .g-m-6, .g-m-7, .g-m-8, .g-m-9, .g-m-10, .g-m-11, .g-m-12');
     const closeBtns = document.querySelectorAll('.galery-close');
-
+    
+    // Abrir e fechar as modais
     function hideAllWindows() {
         modals.forEach(function(modal) {
             modal.classList.remove('show');
@@ -40,6 +41,45 @@ document.addEventListener("DOMContentLoaded", function() {
         modals.forEach(function(modal) {
             if (event.target === modal) {
                 hideAllWindows();
+            }
+        });
+    });
+
+    // Carrossel
+    let currentIndex = 0;
+
+    modals.forEach(function(modal) {
+        const nextButton = modal.querySelector('.next');
+        const prevButton = modal.querySelector('.prev');
+        const carousel = modal.querySelector('.carousel');
+        const images = carousel.querySelectorAll('div');
+        const totalImages = images.length;
+
+        function showImage(index) {
+            if (index < 0) {
+                currentIndex = totalImages - 1;
+            } else if (index >= totalImages) {
+                currentIndex = 0;
+            } else {
+                currentIndex = index;
+            }
+            carousel.style.transform = `translateX(-${currentIndex * 100}%)`; 
+        }
+
+        nextButton.addEventListener('click', () => {
+            showImage(currentIndex + 1);
+        });
+
+        prevButton.addEventListener('click', () => {
+            showImage(currentIndex - 1);
+        });
+
+        // Navegação com as teclas do teclado
+        window.addEventListener('keydown', (event) => {
+            if (event.key === 'ArrowRight') {
+                showImage(currentIndex + 1); 
+            } else if (event.key === 'ArrowLeft') { 
+                showImage(currentIndex - 1);
             }
         });
     });
