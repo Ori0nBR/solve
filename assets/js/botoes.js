83cde2ed96ac365b3f9af304fe21b8ca3d5d35d9
Ori0nBR <168296405+Ori0nBR@users.noreply.github.com>
2025-02-10 12:50:37 -0300

    12:50

--- assets/js/botoes.js
+++ assets/js/botoes.js
@@ -3,12 +3,12 @@ function toggleDiv() {
     var icon = document.getElementById("up-down");
 
     if (div.style.display === "none" || div.style.display === "") {
-        div.style.display = "block";  // Exibe a div
-        icon.classList.remove("fa-angle-down"); // Remove seta para baixo
-        icon.classList.add("fa-angle-up"); // Adiciona seta para cima
+        div.style.display = "block";  
+        icon.classList.remove("fa-angle-down");
+        icon.classList.add("fa-angle-up"); 
     } else {
-        div.style.display = "none";   // Oculta a div
-        icon.classList.remove("fa-angle-up"); // Remove seta para cima
-        icon.classList.add("fa-angle-down"); // Adiciona seta para baixo
+        div.style.display = "none";   
+        icon.classList.remove("fa-angle-up"); 
+        icon.classList.add("fa-angle-down"); 
     }
 }
