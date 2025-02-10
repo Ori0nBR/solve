7a8c49132307f0a28d680739af0b6a890bfa0df3
Ori0nBR <168296405+Ori0nBR@users.noreply.github.com>
2025-02-10 12:48:49 -0300

    botao snos 12:48

--- assets/js/botoes.js
+++ assets/js/botoes.js
@@ -1,9 +1,14 @@
 function toggleDiv() {
     var div = document.getElementById("back");
-    if (div.style.display === "none") {
-        div.style.display = "block"; // Exibe a div
-    } 
-    else {
-        div.style.display = "none";  // Oculta a div
+    var icon = document.getElementById("up-down");
+
+    if (div.style.display === "none" || div.style.display === "") {
+        div.style.display = "block";  // Exibe a div
+        icon.classList.remove("fa-angle-down"); // Remove seta para baixo
+        icon.classList.add("fa-angle-up"); // Adiciona seta para cima
+    } else {
+        div.style.display = "none";   // Oculta a div
+        icon.classList.remove("fa-angle-up"); // Remove seta para cima
+        icon.classList.add("fa-angle-down"); // Adiciona seta para baixo
     }
 }
