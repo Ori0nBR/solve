b99632eda781f4542ae7e1f5f9403b9baccef97f
victordeo <victorde.oliveira@icloud.com>
2025-02-11 00:06:08 -0300

    00:06

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const photos = document.querySelectorAll('.section-projects--photo');
     const modals = document.querySelectorAll('.g-m-1, .g-m-2, .g-m-3, .g-m-4, .g-m-5, .g-m-6, .g-m-7, .g-m-8, .g-m-9, .g-m-10, .g-m-11, .g-m-12');
     const closeBtns = document.querySelectorAll('.galery-close');
-    
+
     function hideAllWindows() {
         modals.forEach(function(modal) {
             modal.classList.remove('show');
@@ -25,8 +25,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function resetCarousel(modal) {
         const carousel = modal.querySelector('.carousel');
-        const images = carousel.querySelectorAll('div');
-        currentIndex = 0;
+        const images = carousel.querySelectorAll('.carousel-item');
+        modal.dataset.currentIndex = 0;
         carousel.style.transform = `translateX(0%)`;
     }
 
@@ -52,14 +52,14 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
-    let currentIndex = 0;
-
     modals.forEach(function(modal) {
         const nextButton = modal.querySelector('.next');
         const prevButton = modal.querySelector('.prev');
         const carousel = modal.querySelector('.carousel');
-        const images = carousel.querySelectorAll('div');
+        const images = carousel.querySelectorAll('.carousel-item');
         const totalImages = images.length;
+
+        let currentIndex = 0;
 
         function showImage(index) {
             if (index < 0) {
@@ -69,7 +69,9 @@ document.addEventListener("DOMContentLoaded", function() {
             } else {
                 currentIndex = index;
             }
-            carousel.style.transform = `translateX(-${currentIndex * 100}%)`; 
+
+            carousel.style.transition = 'transform 0.6s ease';
+            carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
         }
 
         nextButton.addEventListener('click', () => {
@@ -82,7 +84,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
         window.addEventListener('keydown', (event) => {
             if (event.key === 'ArrowRight') {
-                showImage(currentIndex + 1); 
+                showImage(currentIndex + 1);
             } else if (event.key === 'ArrowLeft') { 
                 showImage(currentIndex - 1);
             }
